6780ed86319b5d512eadbbd12bef09ca1b3cc276
Papp-Jancsurák Judit <papp-jancsurakj@KKSZKI.LOCAL>
2022-12-13 17:03:24 +0100

    1. feladat kesz

--- Python/feladatok2.py
+++ Python/feladatok2.py
@@ -9,11 +9,23 @@ def feladat0():
         szam=int(input("Adjon meg egy 20-nál nem nagyobb, pozitív egész számot: "))
     print(szam*" "+"START")
 
-feladat0()
+#feladat0()
 
 """1. Írj egy Python programot, amely bekér egy pozitív egész számot a felhasználótól és kiírja a
 képernyőre azt a számot, amely az ennél a számnál nem nagyobb pozitív egész számok
 összege! """
+
+def feladat1():
+    szam=int(input("Adjon meg egy pozitív egész számot: "))
+    tomb=[]
+    for i in range(szam):
+        tomb.append(szam)
+        szam-=1
+    print(tomb)
+    print(sum(tomb))
+
+feladat1()       
+
 
 """2. Írj egy Python programot, amely bekér egy szót (sztringet) a felhasználótól és kiírja a
 képernyőre a szó betűit, úgy, hogy minden betű egy új sorba kerüljön!""" 
